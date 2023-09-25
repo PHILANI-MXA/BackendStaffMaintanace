f8aefc2e0e3cd13d6d49fce37edd1b96ce9fb287
PHILANI-MXA <3911755@MYUWC.AC.ZA>
2023-09-25 18:31:16 +0200

    changes

--- controllers/employees.js
+++ controllers/employees.js
@@ -3,6 +3,10 @@ router = express.Router();
 
 const db = require('../db');
 
+const getAllEmployees = async ()=>{
+
+}
+
 router.get('/', async (req, res) => {
     const rows = await db.query("SELECT * FROM PersonT")
         .then(data => res.send(data))
--- services/services.js
+++ services/services.js
@@ -3,11 +3,17 @@ const db = require('../db');
 
 router = express.Router();
 
-router.get('/', async (req, res) => {
-    const rows = await db.query("SELECT * FROM EmployeesT")
-        .then(data => res.send(data))
-        .catch(err => console.log (err))
-        console.log(rows)
-})
+module.exports.getAllEmployees = async ()=>{
+    const [rows] = await db.query("SELECT * FROM EmployeesT")
+    .catch(err => console.log (err))
+    return rows;
+}
+
+// router.get('/', async (req, res) => {
+//     const rows = await db.query("SELECT * FROM EmployeesT")
+//         .then(data => res.send(data))
+//         .catch(err => console.log (err))
+//         console.log(rows)
+// })
 
 module.exports = router;
